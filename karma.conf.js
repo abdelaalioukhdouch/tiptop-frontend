0e94591d2e88843177bffcefff8106beb71119b3
abdelaalioukhdouch <62260567+abdelaalioukhdouch@users.noreply.github.com>
2024-05-16 14:52:12 +0200

    tests

--- karma.conf.js
+++ karma.conf.js
@@ -20,6 +20,14 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+
+    reporters: ['progress', 'kjhtml', 'junit'], // Make sure 'junit' is included
+    junitReporter: {
+      outputDir: 'test-results', // This is where your test results XML files will be saved
+      outputFile: 'test-results.xml', // Name of the file to create
+      useBrowserName: false // Set to false unless you need browser name in the file name
+    },
+
     reporters: ['progress', 'kjhtml', 'coverage'], // 'kjhtml' for HTML reports
     port: 9876,
     colors: true,
