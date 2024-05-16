9928914a6192a7cf8cee2a8bbe6ce32dd18aae05
abdelaalioukhdouch <62260567+abdelaalioukhdouch@users.noreply.github.com>
2024-05-16 13:53:55 +0200

    test

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -13,26 +10,17 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated failures
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/projectdsp'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      dir: require('path').join(__dirname, './coverage/tiptipfront'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'coverage'], // 'kjhtml' for HTML reports
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
