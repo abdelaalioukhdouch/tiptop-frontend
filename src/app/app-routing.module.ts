40c5421b406ef5d9565c7dd36c439bf91ab45482
abdelaalioukhdouch <62260567+abdelaalioukhdouch@users.noreply.github.com>
2024-05-23 13:15:47 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -54,6 +54,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     //initialNavigation: 'enabledBlocking'
+    useHash: false
 })],
   exports: [RouterModule],
 })
