f2fe750b469b2d9148c6d0651236d3b26402d44c
abdelaalioukhdouch <62260567+abdelaalioukhdouch@users.noreply.github.com>
2024-05-28 13:51:33 +0200

    rapport test

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,4 @@
+// karma.conf.js
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -6,34 +7,29 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated failures
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tiptipfront'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
       fixWebpackSourcePaths: true
     },
-
-    reporters: ['progress', 'kjhtml', 'junit'], // Make sure 'junit' is included
     junitReporter: {
-      outputDir: 'test-results', // This is where your test results XML files will be saved
-      outputFile: 'test-results.xml', // Name of the file to create
-      useBrowserName: false // Set to false unless you need browser name in the file name
+      outputDir: 'coverage', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'test-results.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      useBrowserName: false // add browser name to report and classes names
     },
-
-    reporters: ['progress', 'kjhtml', 'coverage'], // 'kjhtml' for HTML reports
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
